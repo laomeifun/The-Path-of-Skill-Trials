9708381cf0cbb00f38967bcebb5159823f393a38
zero <zero@laomei.site>
2025-03-30 09:28:56 +0800

    feat: 重构样式文件，合并并优化CSS，更新组件引用路径

--- day00_profileportal/src/components/App.tsx
+++ day00_profileportal/src/components/App.tsx
@@ -1,36 +1,32 @@
-import '../assets/App.css'
+import '../styles/App.css'
 
 
 import Card from './card';
 
 function App() {
   return (
-    <>
-      <div className="app">
-        <Card 
-          title="个人信息卡片" 
-          content="这是一个示例卡片，展示了用户信息。" 
-          avatarProps={{
-            name: "李四",
-            title: "产品经理",
-            imageUrl: "/src/assets/react.svg",
-            size: "large"
-          }}
-        />
-      </div>
-      <div>
-        <Card 
-          title="个人信息卡片" 
-          content="这是一个示例卡片，展示了用户信息。" 
-          avatarProps={{
-            name: "张三",
-            title: "前端开发工程师",
-            imageUrl: "/src/assets/react.svg",
-            size: "large"
-          }}
-        />
-      </div>
-    </>
+    <div className="app-container">
+      <Card 
+        title="个人信息卡片" 
+        content="这是一个示例卡片，展示了用户信息。" 
+        avatarProps={{
+          name: "李四",
+          title: "产品经理",
+          imageUrl: "/src/assets/react.svg",
+          size: "large"
+        }}
+      />
+      <Card 
+        title="个人信息卡片" 
+        content="这是一个示例卡片，展示了用户信息。" 
+        avatarProps={{
+          name: "张三",
+          title: "前端开发工程师",
+          imageUrl: "/src/assets/react.svg",
+          size: "large"
+        }}
+      />
+    </div>
   );
 }
 
